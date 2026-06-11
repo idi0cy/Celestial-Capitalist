b7b522cc160f3bd6b22e1a38624c80da57832123
Kayden Zhou <kaydenzhou@gmail.com>
2026-06-10 23:01:35 -0700

    haggle minigame part basically done possibly
    read the title

---
+++ celestial-capitalist/Scripts/RockBottom/minigames/haggle_target.gd
@@ -0,0 +1,31 @@
+extends TextureButton
+
+var type = "good"
+var time = 1
+#signal uhhPressedBro(magnitude)
+
+@onready var haggle = get_node("../../../Haggle")
+
+func initiate(incType, magnitude):
+	#this is so vestigial but i'm keeping it anyways
+	type = incType
+	time = (1 / magnitude)
+	if type == "good":
+		z_index = 8
+	else:
+		z_index = 5
+	self.pressed.connect(gotPressed)
+	await get_tree().create_timer(time).timeout
+	if incType == "good":
+		haggle.progress -= 5 * magnitude / 2
+	get_parent().remove_child(self)
+	self.queue_free()
+
+func gotPressed():
+	var temp = 1/time
+	if type == "good":
+		haggle.progress += 5 * temp
+	else:
+		haggle.progress -= 5 * temp
+	get_parent().remove_child(self)
+	self.queue_free()
