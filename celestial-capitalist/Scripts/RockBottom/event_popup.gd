b203bcda49b19b7100b7f7083bbaf3e497023677
RatseerOfRattesse <ratseerofrattesse@gmail.com>
2026-09-03 13:41:05 -0700

    Update event_popup.gd

--- celestial-capitalist/Scripts/RockBottom/event_popup.gd
+++ celestial-capitalist/Scripts/RockBottom/event_popup.gd
@@ -5,16 +5,13 @@ signal closed
 
 func initiate(text):
 	eventDesc.text = text
-	print(text)
 
 func _on_back_button_pressed() -> void:
 	get_parent().remove_child(self)
 	closed.emit()
-	print(eventDesc.text)
 	self.queue_free()
 
 func _on_interactable_pressed() -> void:
 	get_parent().remove_child(self)
 	closed.emit()
-	print(eventDesc.text)
 	self.queue_free()
