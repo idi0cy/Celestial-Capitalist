2a113d341f28880f8e2b061c09058a99a4e1acfc
Kayden Zhou <kaydenzhou@gmail.com>
2026-09-12 17:59:23 -0700

    title screen buttons make noise

---
+++ celestial-capitalist/Scripts/TitleScreen/TitleButtonScripts/button_click_noised.gd
@@ -0,0 +1,19 @@
+extends AudioStreamPlayer
+
+func _on_start_button_switch_scene() -> void:
+	play()
+
+func _on_credits_button_credits_pressed() -> void:
+	play()
+
+func _on_settings_button_open_settings() -> void:
+	play()
+
+func _on_interactable_pressed() -> void:
+	play()
+
+func _on_button_pressed() -> void:
+	play()
+
+func _on_exit_button_pressed() -> void:
+	play()
